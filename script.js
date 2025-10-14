997cd28b904b2ba809432d38acbb39eab2e6014f
jcandioto <jcandioto8911@gmail.com>
2025-10-14 11:35:34 -0700

    Add basic js functions for math operations (add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,27 @@
+// Addends are numbers being added together
+function add(addend1, addend2)
+{
+    return addend1 + addend2;
+}
+
+// Minuend is the number being subtracted from, 
+// subtrahend is the number being subtracted
+function subtract(minuend, subtrahend)
+{
+    return minuend - subtrahend;
+}
+
+// Multiplicand is number being multiplied, 
+// multiplier shows how many times it is multiplied
+function multiply(multiplicand, multiplier)
+{
+    return multiplicand * multiplier;
+}
+
+// Dividend is the number being divided, 
+// divisor is number you are dividing by
+function divide(dividend, divisor)
+{
+    return dividend / divisor;
+}
+
